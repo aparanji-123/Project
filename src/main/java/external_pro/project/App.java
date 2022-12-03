638ab748394bb353acb52d0c775ff442bb94f4db
APARANJI <APARANJI@LAPTOP-NVIS5OT0>
2022-12-03 11:57:19 +0530

    This is our project.

---
+++ src/main/java/external_pro/project/App.java
@@ -0,0 +1,18 @@
+package external_pro.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int input=5,i;
+		
+		for(i=1;i<=12;i++)
+		{
+			System.out.printf("%d * %d = %d\n",input,i,(input*i));
+	    }
+    }
+}
